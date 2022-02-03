d430f1e3f0819c70cc1e1b3d2a62f6e7436f4b34
Anton <vsobolev45@gmail.com>
2022-02-03 17:25:10 +0300

    added Ex4

---
+++ lab1/Ex4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cin >> a >> b;
+    if(b > a){
+        swap(a, b);
+    }
+    while (b != 0){
+        a = a%b;
+        swap(a, b);
+    }
+    cout << a;
+    return 0;
+}
