16bd3fa55e4a103270a4d55a70c427536642b1fd
Anton <vsobolev45@gmail.com>
2022-09-16 12:56:21 +0400

    aaa

---
+++ 3sem/classwork02/fraction.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdlib>
+
+class Fraction {
+    public:
+        Fraction(int numerator, int denominator) : numerator(numerator), 
+                                                   denumerator(denumerator){}
+        
+        Fraction(int numerator) : Fraction(numerator, 1) { }
+        Fraction() = delete; 
+    
+        Fraction(const Fraction& other) : numerator(other.numerator), 
+                                          denominator(other.denominator){}
+    
+        void printFraction() const {
+            std::cout << numerator << " / " << denominator << std::endl;
+        }
+
+        void fracReverse(){
+            int tmp = numerator;
+            numerator = denominator;
+            denominator = tmp;
+        }
+
+        void fracSimplify() {
+            for(int i = std::min(numerator, denominator)/2 + 1; i >= 2; i--){
+                if(numerator % i == 0 and denominator % i == 0){
+                    numerator /= i;
+                    denominator /= i;
+                    break;
+                }
+            }
+        }
+
+        void multByNumber(int value){
+            numerator *= value;
+        }
+        
+        void multByFrac(const Fraction& other){
+            numerator *= other.numerator;
+            denominator *= other.denominator;
+        }
+
+        Fraction multByFracReturn(const Fraction& other) const{
+            return Fraction(numerator*other.numerator, denominator*other.denpminator);
+        }
+        
+        double calc() {
+            return static_cast<double>(numerator) / static_cast<double>(denominator);
+        }
+
+        int getNumerator() {
+            return numerator;
+        }
+
+        int getDenominator() {
+            return denominator;
+        }
+    
+    private:
+        int numerator;
+        int denominator;
+};
---
+++ 3sem/classwork02/matrix.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+int calcDeterminantArray(int** arr, int n){
+    if(n==1) return arr[0][0];
+    else{
+        int res = 0;
+        for(int i = 0; i < n; i++){
+            int** minor = new int*[n - 1];
+            for(int j = 0; j < n - 1; j++){
+                minor[j] = new int[n - 1];
+            }
+            for(int j= 1; j < n; j++){
+                int g = 0;
+                for(int k = 0; k < n; k++){
+                    if(k != i){
+                        minor[j - 1][g] = arr[j][k];
+                        g++;
+                    }
+                }
+            }
+            res += arr[0][i]*std::pow(-1, i)*calcDeterminantArray(minor, n - 1);
+            for(int i = 0; i < n - 1; i++){
+                delete[] minor[i];
+            }
+            delete[] minor;   
+        }
+        return res;
+    }
+}
+
+class Matrix {
+    public:
+        Matrix(int arr[], size_t n_elements) : size(std::sqrt(n_elements)){
+            mat = new int*[size];
+            for(int i = 0; i < size; i++){
+                mat[i] = new int[size];
+                for(int j = 0; j < size; j++){
+                    mat[i][j] = arr[i*size + j];
+                }
+            }
+        }
+
+        Matrix(int element, size_t n_elements) : size(std::sqrt(n_elements)){
+            mat = new int*[size];
+            for(int i = 0; i < size; i++){
+                mat[i] = new int[size];
+                for(int j = 0; j < size; j++){
+                    mat[i][j] = element;
+                }
+            }
+        }
+
+        Matrix() : size(1){
+            mat = new int*[1];
+            mat[0] = new int[1];
+            mat[0][0] = 1;
+        }
+
+        Matrix(const Matrix& other) : size(other.size){
+            mat = new int*[size];
+            for(int i = 0; i < size; i++){
+                mat[i] = new int[size];
+                for(int j = 0; j < size; j++){
+                    mat[i][j] = other.mat[i][j];
+                }
+            }
+        }
+
+        void printMatrix() const{
+            for(int i = 0; i < size; i++){
+                for(int j = 0; j < size; j++){
+                    std::cout << mat[i][j] << " ";
+                }
+                std::cout << std::endl;
+            }
+        }
+    
+        int calcDeterminant(){
+            return calcDeterminantArray(mat, size);
+        }
+    
+        void calcSquare() {
+            int** res_mat = new int*[size];
+            for(int i = 0; i < size; i++){
+                res_mat[i] = new int[size];
+                for(int j = 0; j < size; j++){
+                    int curr_field = 0;
+                    for(int p = 0; p < size; p++){
+                        curr_field += mat[i][p] *mat[p][j];
+                    }
+                    res_mat[i][j] = curr_field;
+                }
+            }
+            for(int i = 0; i < size; i++){
+                std::memcpy(mat[i], res_mat[i], size*4);
+                delete[] res_mat[i];
+            }
+            delete[] res_mat;
+            
+        }
+
+        void matTranspose(){
+            for(int i = 0; i < size; i++){
+                for(int j = i; j < size; j++){
+                    std::swap(mat[i][j], mat[j][i]);
+                }
+            }
+        }
+
+        ~Matrix(){
+            for(int i = 0; i < size; i++){
+                delete[] mat[i];
+            }
+            delete[] mat;
+        }
+
+    private:
+        int** mat;
+        int size;
+
+        
+};
+
+int main(){
+    int array[16] = {0, 1, 2, 3, 4, 5, 6, 1, 8, 9, 7, 6, 5, 4, 3, 2};
+    Matrix my_mat = Matrix(array, 16);
+    my_mat.printMatrix();
+    std::cout << my_mat.calcDeterminant() << std::endl;
+    return 0;
+}
